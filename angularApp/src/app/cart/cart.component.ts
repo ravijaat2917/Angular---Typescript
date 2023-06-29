935f969edb0a36db73a9f80e7c15689673848d18
Ravi Lour <ravijaat2917@gmail.com>
2023-06-29 08:57:23 +0530

    databinding and eventbinding code added

--- angularApp/src/app/cart/cart.component.ts
+++ angularApp/src/app/cart/cart.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
+  componentName = 'Cart Component';
+  btnDisabled = true;
+  item = '';
+  cart: any = [];
+  constructor() {
+    setTimeout(() => {
+      this.btnDisabled = false;
+    }, 2000);
+  }
+  getItem(e: any) {
+    console.log(e.target.value);
+    this.item = e.target.value;
+  }
 
+  addToCart() {
+    this.cart.push(this.item);
+    this.item = '';
+  }
 }
